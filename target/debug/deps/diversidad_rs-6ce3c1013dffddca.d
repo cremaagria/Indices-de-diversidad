4b6a65df3bcaa54584934d9a37492882847bcca9
cremaagria <zarazafelipe@gmail.com>
2025-10-11 22:32:26 -0500

    fourth commit

---
+++ target/debug/deps/diversidad_rs-6ce3c1013dffddca.d
@@ -0,0 +1,5 @@
+/home/andres/Escritorio/Indices-de-diversidad/target/debug/deps/diversidad_rs-6ce3c1013dffddca.d: src/main.rs
+
+/home/andres/Escritorio/Indices-de-diversidad/target/debug/deps/libdiversidad_rs-6ce3c1013dffddca.rmeta: src/main.rs
+
+src/main.rs:
